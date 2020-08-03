b70ee8a734c87ac8c7ae14f3c6b924f57d8d2e9d
zenit70 <fulvio.zanetti@gmail.com>
2020-08-03 12:12:45 +0200

    Added id_helper test

---
+++ src/id/__tests__/id_helper.spec.js
@@ -0,0 +1,98 @@
+import {
+  buildRequestDidPowerUpMsgList,
+  buildSetDidDocumentMsgList
+} from "../id_helper";
+
+describe("id/id__helper", () => {
+  it("Checks if function 'buildRequestDidPowerUpMsgList' returns an array of 'commercio/MsgRequestDidPowerUp'", () => {
+    const didPowerUpRequests = [{
+      claimant: "address that sent funds to the centralized entity before",
+      amount: [{
+        denom: "ucommercio",
+        amount: "amount to transfer to the pairwise did, integer"
+      }],
+      proof: "proof string",
+      id: "randomly-generated UUID v4",
+      proof_key: "proof encryption key"
+    }];
+
+    const expectedValue = [{
+      type: "commercio/MsgRequestDidPowerUp",
+      value: {
+        claimant: "address that sent funds to the centralized entity before",
+        amount: [{
+          denom: "ucommercio",
+          amount: "amount to transfer to the pairwise did, integer"
+        }],
+        proof: "proof string",
+        id: "randomly-generated UUID v4",
+        proof_key: "proof encryption key"
+      }
+    }];
+
+    const response = buildRequestDidPowerUpMsgList(didPowerUpRequests);
+
+    expect(response).toStrictEqual(expectedValue);
+  });
+
+  it("Checks if function 'buildSetDidDocumentMsgList' returns an array of 'commercio/MsgSetIdentity'", () => {
+    const didDocuments = [{
+      "@context": "https://www.w3.org/ns/did/v1",
+      id: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+      publicKey: [{
+          id: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc#keys-1",
+          type: "RsaVerificationKey2018",
+          controller: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+          publicKeyPem: "-----BEGIN PUBLIC KEY----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDMr3V+Auyc+zvt2qX+jpwk3wM+m2DbfLjimByzQDIfrzSHMTQ8erL0kg69YsXHYXVX9mIZKRzk6VNwOBOQJSsIDf2jGbuEgI8EB4c3q1XykakCTvO3Ku3PJgZ9PO4qRw7QVvTkCbc91rT93/pD3/Ar8wqd4pNXtgbfbwJGviZ6kQIDAQAB-----END PUBLIC KEY-----\r\n"
+        },
+        {
+          id: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc#keys-2",
+          type: "RsaSignatureKey2018",
+          controller: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+          publicKeyPem: "-----BEGIN PUBLIC KEY----MIGfM3TvO3Ku3PJgZ9PO4qRw7+Auyc+zvt2qX+jpwk3wM+m2DbfLjimByzQDIfrzSHMTQ8erL0kg69YsXHYXVX9mIZKRzk6VNwOBOQJSsIDf2jGbuEgI8EB4c3q1XykakCQVvTkCbc9A0GCSqGSIbqd4pNXtgbfbwJGviZ6kQIDAQAB-----END PUBLIC KEY-----\r\n"
+        }
+      ],
+      proof: {
+        type: "EcdsaSecp256k1VerificationKey2019",
+        created: "2019-02-08T16:02:20Z",
+        proofPurpose: "authentication",
+        controller: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+        verificationMethod: "<did bech32 pubkey>",
+        signatureValue: "QNB13Y7Q91tzjn4w=="
+      }
+    }];
+
+    const expectedValue = [{
+      type: "commercio/MsgSetIdentity",
+      value: {
+        "@context": "https://www.w3.org/ns/did/v1",
+        id: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+        publicKey: [{
+            id: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc#keys-1",
+            type: "RsaVerificationKey2018",
+            controller: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+            publicKeyPem: "-----BEGIN PUBLIC KEY----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDMr3V+Auyc+zvt2qX+jpwk3wM+m2DbfLjimByzQDIfrzSHMTQ8erL0kg69YsXHYXVX9mIZKRzk6VNwOBOQJSsIDf2jGbuEgI8EB4c3q1XykakCTvO3Ku3PJgZ9PO4qRw7QVvTkCbc91rT93/pD3/Ar8wqd4pNXtgbfbwJGviZ6kQIDAQAB-----END PUBLIC KEY-----\r\n"
+          },
+          {
+            id: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc#keys-2",
+            type: "RsaSignatureKey2018",
+            controller: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+            publicKeyPem: "-----BEGIN PUBLIC KEY----MIGfM3TvO3Ku3PJgZ9PO4qRw7+Auyc+zvt2qX+jpwk3wM+m2DbfLjimByzQDIfrzSHMTQ8erL0kg69YsXHYXVX9mIZKRzk6VNwOBOQJSsIDf2jGbuEgI8EB4c3q1XykakCQVvTkCbc9A0GCSqGSIbqd4pNXtgbfbwJGviZ6kQIDAQAB-----END PUBLIC KEY-----\r\n"
+          }
+        ],
+        proof: {
+          type: "EcdsaSecp256k1VerificationKey2019",
+          created: "2019-02-08T16:02:20Z",
+          proofPurpose: "authentication",
+          controller: "did:com:14zk9u8894eg7fhgw0dsesnqzmlrx85ga9rvnjc",
+          verificationMethod: "<did bech32 pubkey>",
+          signatureValue: "QNB13Y7Q91tzjn4w=="
+        }
+      }
+    }];
+
+    const response = buildSetDidDocumentMsgList(didDocuments);
+
+    expect(response).toStrictEqual(expectedValue);
+  });
+});
